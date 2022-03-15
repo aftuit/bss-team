8e1bb3f5ea248212b21967b5a7ca68605a75bf4b
bssteamdeveloper <bssteamdeveloper123@gmail.com>
2022-03-15 12:14:19 +0500

    libraries installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import Navbar from "./components/Navbar/Navbar";
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home/Home";
 function App() {
   return (
-    <div className="App">
-      react app
-    </div>
+      <div>
+          <Navbar/>
+          <Routes>
+              <Route path={"/"} element={<Home/>}/>
+          </Routes>
+      </div>
   );
 }
 
